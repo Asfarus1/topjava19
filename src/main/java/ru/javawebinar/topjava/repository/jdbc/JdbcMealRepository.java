f2e3db42c84d89122c4c563c9ca3d1b035041e11
Asfarus1 <m.a.v.norm@mail.ru>
2020-04-20 21:28:41 +0300

    JdbcMealRepository and fix tests

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -1,37 +1,77 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JdbcMealRepository implements MealRepository {
+    private static final BeanPropertyRowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+    private static final String UPDATE_QUERY = "UPDATE meals SET date_time=:date_time, description=:description, calories=:calories WHERE id=:id AND user_id=:user_id";
+    private static final String SELECT_HALF_BETWEEN_QUERY = "SELECT * FROM meals WHERE user_id=:userId AND date_time>=:startDate AND date_time<:endDate ORDER BY date_time DESC";
+
+    private final NamedParameterJdbcTemplate namedTemplate;
+    private final SimpleJdbcInsert insert;
+
+    public JdbcMealRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.namedTemplate = jdbcTemplate;
+        insert = new SimpleJdbcInsert(jdbcTemplate.getJdbcTemplate())
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id");
+    }
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        if (meal.isNew()) {
+            Number id = insert.executeAndReturnKey(toParameters(meal, userId));
+            meal.setId(id.intValue());
+        } else {
+            namedTemplate.update(UPDATE_QUERY, toParameters(meal, userId));
+        }
+        return meal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return namedTemplate.getJdbcTemplate().update(
+                "DELETE FROM meals WHERE id=? AND user_id=?", id, userId) > 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return namedTemplate.getJdbcTemplate().queryForObject(
+                "SELECT * FROM meals WHERE id=? AND user_id=?", ROW_MAPPER, id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return namedTemplate.getJdbcTemplate().query(
+                "SELECT * FROM meals WHERE user_id=? ORDER BY date_time DESC", ROW_MAPPER, userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("userId", userId)
+                .addValue("startDate", startDate)
+                .addValue("endDate", endDate);
+        return namedTemplate.query(SELECT_HALF_BETWEEN_QUERY, params, ROW_MAPPER);
+    }
+
+    private static Map<String,Object> toParameters(Meal meal, int userId) {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String,Object> values = mapper.convertValue(meal, Map.class);
+        values.put("user_id", userId);
+        values.put("date_time", meal.getDateTime());
+        return values;
     }
 }
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,29 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2020-01-30 10:00', 'Завтрак', 500, 100000),
+       ('2020-01-30 13:00', 'Обед', 1000, 100000),
+       ('2020-01-30 20:00', 'Ужин', 500, 100000),
+       ('2020-01-31 00:00', 'Еда на граничное значение', 100, 100000),
+       ('2020-01-31 10:00', 'Завтрак', 1000, 100000),
+       ('2020-01-31 13:00', 'Обед', 500, 100000),
+       ('2020-01-31 20:00', 'Ужин', 410, 100000);
+
+UPDATE meals
+SET date_time='2020-01-31 00:00',
+    description='Еда на граничное значение',
+    calories=101
+WHERE id = 100005
+  AND user_id = 100000
